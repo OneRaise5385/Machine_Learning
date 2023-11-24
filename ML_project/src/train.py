5c89c6e8515ea7e0bdcccd1ef77ce9784b39ba6b
OneRaise <zh1372337794@163.com>
2023-11-24 21:37:44 +0800

    update

--- ML_project/src/train.py
+++ ML_project/src/train.py
@@ -5,4 +5,35 @@ from sklearn import tree
 
 def run(fold):
     # 读取数据文件
-    df = pd.read_table('../input/')
+    df = pd.read_table('../input/mnist_train_folds.csv')
+
+    # 划分训练集与测试集
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_test = df[df.kfold == fold].reset_index(drop=True)
+
+    # 训练集特征标签分离
+    x_train = df_train.drop('label', axis=1).values
+    y_train = df_train.label.values
+
+    # 测试集特征标签分离
+    x_test = df_test.drop('label', axis=1).values
+    y_test = df_test.label.values
+
+    # 决策树模型
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(x_train, y_train)
+    preds = clf.predict(x_test)
+
+    # 计算准确率
+    accuracy = metrics.accuracy_score(y_test, preds)
+    print(f'Folf={fold}', Accuracy={accuracy})
+
+    # 保存模型
+    joblib.dump(clf, f'../models/df_{fold}.bin')
+
+if __name__ == '__main__':
+    run(fold=0)
+    run(fold=1)
+    run(fold=2)
+    run(fold=3)
+    run(fold=4)
