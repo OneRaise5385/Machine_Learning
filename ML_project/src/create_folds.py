4dd53ce49820b03b47aac7154934a65053edaf5b
OneRaise <zh1372337794@163.com>
2023-11-25 14:02:13 +0800

    update

---
+++ ML_project/src/create_folds.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == '__main__':
+    # 导入数据
+    df_x = np.loadtxt('ML_project/input/mnist_x')
+    df_y = np.loadtxt('ML_project/input/mnist_y')
+    df = pd.DataFrame(df_x)
+    df['label'] = df_y
+    df['kfold'] = -1
+
+    # 随机打乱数据
+    df = df.sample(frac=1).reset_index(drop=True)
+    kf = model_selection.KFold(n_splits=5)
+    for fold, (trn_, val_) in enumerate(kf.split(X=df)):
+        df.loc[val_, 'kfold'] = fold
+    # 保存新的数据集
+    df.to_csv('ML_project/input/mnist.csv', index=False)
--- ML_project/src/train.py
+++ ML_project/src/train.py
@@ -5,11 +5,11 @@ from sklearn import tree
 
 def run(fold):
     # 读取数据文件
-    df = pd.read_table('../input/mnist_train_folds.csv')
+    df = pd.read_csv('ML_project/input/mnist.csv')
 
     # 划分训练集与测试集
-    df_train = df[df.kfold != fold].reset_index(drop=True)
-    df_test = df[df.kfold == fold].reset_index(drop=True)
+    df_train = df[df['kfold'] != fold].reset_index(drop=True)
+    df_test = df[df['kfold'] == fold].reset_index(drop=True)
 
     # 训练集特征标签分离
     x_train = df_train.drop('label', axis=1).values
@@ -26,10 +26,10 @@ def run(fold):
 
     # 计算准确率
     accuracy = metrics.accuracy_score(y_test, preds)
-    print(f'Folf={fold}', Accuracy={accuracy})
+    print(f'Folf={fold}, Accuracy={accuracy}')
 
     # 保存模型
-    joblib.dump(clf, f'../models/df_{fold}.bin')
+    joblib.dump(clf, f'ML_project/models/df_{fold}.bin')
 
 if __name__ == '__main__':
     run(fold=0)
